1ec0c02a877fd163168acc09de6738dfd671d2f3
kinojetzt <natehiggers613@gmail.com>
2024-08-13 22:30:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// script.js
+
+const apiKey = 'YOUR_TMDB_API_KEY';
+const movieContainer = document.getElementById('movies');
+
+async function fetchMovies() {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`);
+    const data = await response.json();
+    displayMovies(data.results);
+}
+
+function displayMovies(movies) {
+    movieContainer.innerHTML = '';
+
+    movies.forEach(movie => {
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('movie');
+        
+        movieElement.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
+            <div class="movie-info">
+                <h3>${movie.title}</h3>
+                <p>Rating: ${movie.vote_average}</p>
+                <p>${movie.release_date}</p>
+            </div>
+        `;
+
+        movieContainer.appendChild(movieElement);
+    });
+}
+
+// Initialize the movie fetch on page load
+fetchMovies();
